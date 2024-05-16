c32b1065dcd47f9c27d4d35ca46a4f230b4018f9
Philippe MICHEL <philippe@docteur-michel.fr>
2024-05-16 15:08:51 +0200

    Rapport stat V1

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
+![USRC](novo_usrc.png)
+
 <!-- badges: start -->
+
 ![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)
 
 [![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
 <!-- badges: end -->
 
 
 # Petit-Poids
-Étude prospective non interventionnelle
+
+Étude prospective non interventionnelle monocentrique
 
 
 **Plan d'analyse statistique**
+
+**PetitPoids_rapport** rapport statistique pour une sortie en html.
+
+**PetitPoids_rapport2** rapport statistique pour une sortie en pdf en lien avec `_quarto.yaml`.
--- import.R
+++ import.R
@@ -18,7 +18,7 @@ library(labelled)
 
 import1 <- function(fich){
   fich <- paste0("datas/",fich,".ods")
-tt <- read_ods(fich, sheet = 1) |>   
+tt <- read_ods(fich, sheet = 1, na = c("", " ","NA","D","ND","Non disponible")) |>   
     janitor::clean_names() |> 
     # janitor::remove_empty(c("rows", "cols")) |>
     mutate_if(is.character, as.factor) |> 
@@ -30,8 +30,19 @@ return(tt)
 }
 
 importph <- function(){
-inclus <- import1("INCLUSION")
-med <- import1("medecins")
-question <- import1("QUESTIONNAIRE")
-suivi <- import1("suivi")
+inclus <- import1("inclusion")
+med <- import1("medecin")
+quest <- import1("questionnaire")
+suivi <- import1("suivi") |> 
+mutate(mois = as.character(mois)) |> 
+mutate(mois = fct_recode(mois,
+    "mois 2" = "2",
+    "mois 4" = "4",
+    "mois 11" = "11"
+  )) |> 
+  mutate(mois = fct_relevel(mois,
+  "mois 2", "mois 4", "mois 11"
+))
+tt <- left_join(inclus, quest, by = "subjid") 
+save(tt, med, suivi, file = "datas/petitpoids.RData")
 }
