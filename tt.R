80bfd24db2852d035e96550f318cc6fc4d11bd83
Philippe MICHEL <philippe@docteur-michel.fr>
2024-05-31 17:03:26 +0200

    Correction diverses, Z-scores dans le rapport.

--- tt.R
+++
@@ -1,9 +0,0 @@
-suivi |> 
-  drop_na(zscorepds) |> 
-  group_by(mois) |> 
- count(mois)
-
-quest |> 
-  drop_na(q11_rgo_on) |> 
-  group_by(parent) |> 
-  count(q11_rgo_on)
--- xscore .R
+++ xscore .R
@@ -2,7 +2,7 @@
 
 #  ------------------------------------------------------------------------
 #
-# Title : Xscore - création du fichier patients
+# Title : zscore - création du fichier patients
 #    By : 
 #  Date : 2024-05-27
 #    
@@ -27,37 +27,42 @@ suivi <- read_ods("datas/suivi.ods", na = "ND") |>
   janitor::clean_names() |> 
   mutate_if(is.character, as.factor) 
 
-xscore <- left_join(suivi,sex, by = "subjid") |> 
+zscore <- left_join(suivi,sex, by = "subjid") |> 
   drop_na(pds)
 
-save(xscore, file = "datas/xscorex.RData")
+save(zscore, file = "datas/zscorex.RData")
 
 #  ------------------------------------------------------------------------
 # Calcul des zscores - poids
   
 
 
-for (i in 1:nrow(xscore)){
-aa <- which(pd$sexe == xscore$sexe[i] & pd$mois == xscore$mois[i])
-pdtm <- pd$pdm[aa]
-pdtsd <- pd$pdsd[aa]
-xscore$zscorepds[i]   <-  (xscore$pds[i] - pdtm) / pdtsd
+for (i in 1:nrow(zscore)){
+# aa <- which(pd$sexe == zscore$sexe[i] & pd$mois == zscore$mois[i])
+# pdtm <- pd$pdm[aa]
+# pdtsd <- pd$pdsd[aa]
+# zscore$zscorepds[i]   <-  (zscore$pds[i] - pdtm) / pdtsd
+  zscore$zscorepds[i] <- NA
 }
 
 # Calcul des zscores - taille
 
-for (i in 1:nrow(xscore)){
-aa <- which(tail$sexe == xscore$sexe[i] & tail$mois == xscore$mois[i])
+for (i in 1:nrow(zscore)){
+aa <- which(tail$sexe == zscore$sexe[i] & tail$mois == zscore$mois[i])
 tdtm <- tail$tm[aa]
 tdtsd <- tail$tsd[aa] 
-xscore$zscoretaill[i]   <- (xscore$taille[i] - tdtm) / tdtsd
+zscore$zscoretaill[i]   <- (zscore$taille[i] - tdtm) / tdtsd
 }
 
 # Calcul des zscores - périmètre crânien
 
-for (i in 1:nrow(xscore)){
-aa <- which(pc$sexe == xscore$sexe[i] & pc$mois == xscore$mois[i])
+for (i in 1:nrow(zscore)){
+aa <- which(pc$sexe == zscore$sexe[i] & pc$mois == zscore$mois[i])
 pcdtm <- pc$pcm[aa]
 pcdtsd <- pc$pcsd[aa]
-xscore$zscorepc[i] <- (xscore$pc[i] - pcdtm) / pcdtsd
+zscore$zscorepc[i] <- (zscore$pc[i] - pcdtm) / pcdtsd
 }
+
+save(zscore, file = "datas/zscore.RData")
+
+
